044ae480276f9115a51c6bb968f5b2a53537b5c0
Ali Sajid <ali.usmansajid02@gmail.com>
2023-11-14 06:36:54 -0800

    Changing breadcrumb

--- theme/includes/custom-functions.php
+++ theme/includes/custom-functions.php
@@ -18,4 +18,15 @@
         return $match_found;
     }
 
+    add_filter( 'woocommerce_breadcrumb_home_url', 'woo_custom_breadrumb_home_url' );
+    function woo_custom_breadrumb_home_url() {
+        return get_permalink( wc_get_page_id( 'shop' ) );
+    }
+
+    add_filter( 'woocommerce_breadcrumb_defaults', 'wcc_change_breadcrumb_home_text' );
+    function wcc_change_breadcrumb_home_text( $defaults ) {
+        // Change the breadcrumb home text from 'Home' to 'Apartment'
+        $defaults['home'] = 'Shop';
+        return $defaults;
+    }
 ?>
