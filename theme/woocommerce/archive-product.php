01bbc84969353dfe234253cbfe4e57153b77f9b9
Anas Hussain <anashussain2711@gmail.com>
2023-12-06 11:01:21 +0500

    small fixes

--- theme/woocommerce/archive-product.php
+++ theme/woocommerce/archive-product.php
@@ -59,7 +59,7 @@ else{
 }
 ?>
 <header class="woocommerce-products-header">
-	<?php if (( apply_filters( 'woocommerce_show_page_title', true ) ) && ( !is_product_category() )) : ?>
+	<?php if (( apply_filters( 'woocommerce_show_page_title', true ) ) && ( !is_product_category() || is_shop() )) : ?>
 		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
 	<?php endif; ?>
 
