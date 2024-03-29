7491979b82e7301a6d0732f3f3c1ff78ac01bf50
Ali Usman <ali.prystech@gmail.com>
2024-03-29 06:37:10 +0500

    Dynamic header links, cat question pages + fixes
    font sizes spacing, ordering of posts, dropdown icon

--- theme/single-help-support.php
+++ theme/single-help-support.php
@@ -14,12 +14,24 @@
             </div>
             <div class="link-wrap">
                 <div class="links">
-                    <?php if(get_post($main_post_parent)->post_name == "legal"){?>
-                        <a href="/help/help">Help Center</a>            
-                    <?php } else {?>
-                        <a href="/help/legal">Legal</a>
-                    <?php }?>
-                        <a href="/contact">Contact</a>
+                    <?php
+                        $link_args = array(
+                            'post_type' => 'help-support',
+                            'post_status' => 'publish',
+                            'orderby' => 'menu_order',
+                            'order' => 'ASC',
+                            'post_parent' => 0,
+                        );
+                        $links = new WP_Query($link_args);
+                        foreach($links->get_posts() as $link){ 
+                            if($link->ID === $main_post_parent){
+                                continue; 
+                            } else {?>
+                                <a href="<?php echo get_permalink($link); ?>"><?php echo $link->post_title; ?></a>            
+                        <?php } 
+                        }
+                    ?>
+                    <a href="/contact">Contact</a> 
                 </div>
             </div>
         </div>
@@ -42,24 +54,32 @@
                     $args = array(
                         'post_type' => 'help-support',
                         'post_status' => 'publish',
+                        'orderby' => 'menu_order',
+                        'order' => 'ASC',
                         'post_parent' => $main_post_parent,
                     );
                     $posts = new WP_Query($args);
                     foreach($posts->get_posts() as $parent_post){?>
                         <div class="question-wrap">
                             <h3 class="question-category">
-                                <?php echo($parent_post->post_title);?>
+                                <span>
+                                    <?php echo($parent_post->post_title);?>
+                                </span>
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7 10L12 15L17 10" stroke="#5e5e5e" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                             </h3>
-                            <div class="questions">
+                            <div class="questions <?php if ( $post->ID == $parent_post->ID ) { echo "active-parent-post"; } ?>">
                                 <?php
                                     $child_args = array(
                                         'post_type' => 'help-support',
                                         'post_status' => 'publish',
+                                        'orderby' => 'menu_order',
+                                        'order' => 'ASC',
                                         'post_parent' => $parent_post->ID,
                                     );
                                     $child_posts = new WP_Query($child_args);
                                     foreach($child_posts->get_posts() as $child){?>
-                                        <a class="question-item <?php if($post->ID == $child->ID){echo "active-post";}?>" href="<?php echo get_the_permalink($child->ID); ?>">
+                                        <a class="question-item <?php if ( $post->ID == $child->ID ) { echo "active-post"; } ?> " 
+                                        href="<?php echo get_the_permalink($child->ID); ?>">
                                             <img src="<?php echo get_site_url() . "/wp-content/uploads/2024/03/FF-Help_Arrow.svg" ?>" alt="">
                                             <?php
                                                 echo $child->post_title;
@@ -73,7 +93,7 @@
             </div>
             <div class="main-content">
                 <div class="description">
-                    <?php echo get_the_content();?>
+                    <?php echo wpautop($post->post_content);?>
                 </div>
             </div>
         </div>
@@ -85,10 +105,15 @@
 <script>
     $(document).ready(function(){
         $(".active-post").parent(".questions").show()
+        $(".active-post").parent(".questions").parent(".question-wrap").find(".question-category").find("svg").addClass("active")
+        $(".questions.active-parent-post").show()
+        $(".questions.active-parent-post").parent(".question-wrap").find(".question-category").find("svg").addClass("active")
         $(".question-wrap").click(function(){
             if($(this).find(".questions").css("display") == "none"){
+                $(".question-category svg").removeClass("active");
                 $(".question-wrap .questions").slideUp("fast");
                 $(this).find(".questions").slideDown("fast");
+                $(this).find(".question-category svg").addClass("active");
             }
         })
         $(".hide-questions").click(function(){
